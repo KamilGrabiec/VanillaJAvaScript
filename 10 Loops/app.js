7e046e01cc3b75cd4b0f2f72cf30e4e2208efe80
Kamil Grabiec <kamil.grabiec@pl.abb.com>
2021-10-14 13:36:41 +0200

    Section 1 end

---
+++ 10 Loops/app.js
@@ -0,0 +1,28 @@
+//Loops
+//repeatedly run block of code while condition is true
+
+// while loop
+
+var amount = 10;
+
+while (amount > 0) {
+  console.log('I have ' + amount + " dolars and I''m going to the mall.");
+  amount--;
+}
+
+// do while loop
+// First we execute loop, then check condition
+
+var money = 10;
+do {
+  console.log('You have ' + money + ' dolars.');
+  money++;
+} while (money < 10);
+
+// for loop
+
+for (var i = 0; i < 10; i++) {
+  console.log('Number is: ' + i);
+}
+
+console.log(name1);
